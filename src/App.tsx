f52d98ffd2e6c2c8d12b1ef3760bae5263bc79aa
Liam O'Donnell <spikything@gmail.com>
2022-06-27 18:20:41 +0100

    Add mantine and redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Text } from '@mantine/core';
+
+const AddToFirstButton = () => {
+  return <Button size='xl'>Add to first</Button>
+}
+
+const FirstValue = () => {
+  return <Text size='xl' p={5}>
+    First value
+  </Text>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={10}>
+      <Box sx={{
+        display: 'flex',
+      }}>
+        <AddToFirstButton />
+        <FirstValue />
+      </Box>
+    </Box>
   );
 }
 
